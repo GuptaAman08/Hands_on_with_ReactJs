e2329ca6b7620093c4d6656e20064feb636b4549
GuptaAman08 <2015aman.a@ves.ac.in>
2019-08-21 17:36:52 +0530

    Creating react app using npx module create-react-app which is a wireframe for react applications

---
+++ UsingCreateReactAppPackage/my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Ninjas from './Ninjas'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My First React App</h1>
+      <Ninjas />
+    </div>
+  );
+}
+
+export default App;
---
+++ UsingCreateReactAppPackage/my-app/src/Ninjas.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Ninjas(){
+    return (
+        <div className='ninja'>
+            <p>Name : Aman</p>
+            <p>Age: 22</p>
+            <p>Designation: Software Engineer</p>
+        </div>
+    )
+}
+
+export default Ninjas
