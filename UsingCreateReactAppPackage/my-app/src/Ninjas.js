c521646a4b13c3a82e393ac2917cd889254e100d
GuptaAman08 <2015aman.a@ves.ac.in>
2019-08-22 20:10:15 +0530

    Conditional Output in UI component

--- UsingCreateReactAppPackage/my-app/src/Ninjas.js
+++ UsingCreateReactAppPackage/my-app/src/Ninjas.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
 
 // Props are short name for properties, which allows us to pass data from parent component to child component
-class Ninjas extends Component{
-    render(){
-        // Similar to unpacking in python
-        const { name, age, prof } = this.props
-        return(
+const Ninjas = (props) => {
+    // const { ninjas } = props
+    // const NinjaList = ninjas.map(ninja => {
+    //     if (ninja.age > 20){
+    //         return(
+    //             <div className="Ninjas">
+    //                 <p>Name : { ninja.name }</p>
+    //                 <p>Age: { ninja.age }</p>
+    //                 <p>Profression : { ninja.prof }</p>
+    //             </div>    
+    //         )
+    //     }
+    //     else{
+    //         return null
+    //     }
+        
+    // })
+
+    const { ninjas } = props
+    const NinjaList = ninjas.map(ninja => {
+        return ninja.age > 20 ? (
             <div className="Ninjas">
-                <p>Name : {name}</p>
-                <p>Age: { age }</p>
-                <p>Profression : { prof }</p>
-            </div>
-        )
-    }
+                <p>Name : { ninja.name }</p>
+                <p>Age: { ninja.age }</p>
+                <p>Profression : { ninja.prof }</p>
+            </div>    
+            
+        ) : null
+    })
+    return(
+        <div className="NinjaList">
+            { NinjaList }
+        </div>
+    )
+    
 }
 export default Ninjas
